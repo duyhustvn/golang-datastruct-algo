190d721bc0d112deb878e9aa3851e24a08b5e5c8
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2022-02-24 09:43:38 +0700

    array two sum

---
+++ array/easy/two_sum/two_sum.go
@@ -0,0 +1,29 @@
+package section2
+
+// https://leetcode.com/problems/two-sum/
+
+func twoSum(nums []int, sum int) []int {
+	if len(nums) < 2 {
+		return nil
+	}
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == sum {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSumUsingMap(nums []int, sum int) []int {
+	idxMap := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		target := sum - nums[i]
+		if idx, ok := idxMap[target]; ok {
+			return []int{idx, i}
+		}
+		idxMap[nums[i]] = i
+	}
+	return nil
+}
---
+++ array/easy/two_sum/two_sum_test.go
@@ -0,0 +1,57 @@
+package section2
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestData struct {
+	Nums          []int
+	Sum           int
+	ExpectedIndex []int
+}
+
+func generateTestCase() []TestData {
+	return []TestData{
+		{
+			Nums:          []int{1, 3, 7, 9, 2},
+			Sum:           11,
+			ExpectedIndex: []int{3, 4},
+		},
+		{
+			Nums:          []int{1, 3, 7, 9, 2},
+			Sum:           6,
+			ExpectedIndex: nil,
+		},
+		{
+			Nums:          []int{5},
+			Sum:           5,
+			ExpectedIndex: nil,
+		},
+		{
+			Nums:          []int{},
+			Sum:           5,
+			ExpectedIndex: nil,
+		},
+	}
+}
+
+func TestTwoSum(t *testing.T) {
+	tests := generateTestCase()
+	for _, test := range tests {
+		foundIndex := twoSum(test.Nums, test.Sum)
+		if !reflect.DeepEqual(foundIndex, test.ExpectedIndex) {
+			t.Errorf("Expected: %+v instead of %+v", test.ExpectedIndex, foundIndex)
+		}
+	}
+}
+
+func TestTwoSumUsingMap(t *testing.T) {
+	tests := generateTestCase()
+	for _, test := range tests {
+		foundIndex := twoSumUsingMap(test.Nums, test.Sum)
+		if !reflect.DeepEqual(foundIndex, test.ExpectedIndex) {
+			t.Errorf("Expected: %+v instead of %+v", test.ExpectedIndex, foundIndex)
+		}
+	}
+}
