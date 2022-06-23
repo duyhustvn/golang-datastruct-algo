6b607a38b3556467f7b18c1919a14cf77f3ac26e
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2022-06-24 00:15:01 +0700

    add test for enqueue priority queue

--- priority_queue/priority_queue_test.go
+++ priority_queue/priority_queue_test.go
@@ -1,12 +1,13 @@
 package priorityqueue
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEnqueue(t *testing.T) {
-
+	// 6 -> 5 -> 4 -> 2 -> 1 -> 0
 	pq := NewPriorityQueue()
 
 	pq.Enqueue(5, 5)
@@ -33,5 +34,155 @@ func TestEnqueue(t *testing.T) {
 	assert.Equal(t, 6, pq.Front.Value)
 	assert.Equal(t, 0, pq.Rear.Value)
 
-	t.Log(pq.ToString())
+	// t.Log(pq.ToString())
+}
+
+func TestDeEnqueue(t *testing.T) {
+	// 6 -> 5 -> 4 -> 2 -> 1 -> 0
+	pq := NewPriorityQueue()
+
+	var node *Node
+	var ok bool
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+	pq.Enqueue(1, 1)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 1)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+	pq.Enqueue(1, 1)
+	pq.Enqueue(2, 2)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 2)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 1)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+	pq.Enqueue(1, 1)
+	pq.Enqueue(2, 2)
+	pq.Enqueue(6, 6)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 6)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 2)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 1)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+	pq.Enqueue(1, 1)
+	pq.Enqueue(2, 2)
+	pq.Enqueue(6, 6)
+	pq.Enqueue(0, 0)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 6)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 2)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 1)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 0)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	pq.Enqueue(5, 5)
+	pq.Enqueue(1, 1)
+	pq.Enqueue(2, 2)
+	pq.Enqueue(6, 6)
+	pq.Enqueue(0, 0)
+	pq.Enqueue(4, 4)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 6)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 5)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 4)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 2)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 1)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Equal(t, node.Value, 0)
+	assert.True(t, ok)
+
+	node, ok = pq.Dequeue()
+	assert.Nil(t, node)
+	assert.False(t, ok)
+
+	// t.Log(pq.ToString())
+
 }
